a1c87bdda49e808acb7f5039fea54c607b972aab
Alireza Yahyanejad <yahyanejadalre@gmail.com>
2023-11-02 17:46:31 +0100

    main menu updated

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -2,10 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 public class MainMenu : MonoBehaviour
 {
+
     public void PlayGame()
     {
+        // Save the variables here (e.g., PlayerPrefs or a database)
         SceneManager.LoadSceneAsync("Lev1_House");
     }
 
