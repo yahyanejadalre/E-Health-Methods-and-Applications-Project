59111d2701f6973dc8b2820dda3a9a5074284fb7
Alireza Yahyanejad <yahyanejadalre@gmail.com>
2023-11-15 20:16:47 +0100

    codes of Main Menu updated

--- Assets/Script/MainMenu.cs
+++ Assets/Script/MainMenu.cs
@@ -8,14 +8,14 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 [System.Serializable]
-public class Human
+public class Person
 {
     public string name;
     public string gender;
     public int age;
     public string character;
 
-    public Human(string name, string gender, int age, string character)
+    public Person(string name, string gender, int age, string character)
     {
         this.name = name;
         this.gender = gender;
@@ -30,7 +30,7 @@ public class MainMenu : MonoBehaviour
     public TMP_InputField GenderInputFied;
     public TMP_InputField AgeInputFied;
 
-    private List<Human> HumanList = new List<Human>();
+    private List<Person> HumanList = new List<Person>();
     private List<InputField> InputList= new List<InputField>();
     private InputField[] Inputs;
     public void PlayGame()
@@ -57,7 +57,7 @@ public class MainMenu : MonoBehaviour
             character = "error";
         }
         int AgeInputNumber = int.Parse(AgeInputFied.text);
-        var NewHuman = new Human(NameInputFied.text,GenderInputFied.text,AgeInputNumber,character);
+        var NewPerson = new Person(NameInputFied.text,GenderInputFied.text,AgeInputNumber,character);
         Debug.Log("");
         print("name is " + NameInputFied.text);
         print("gender is " + GenderInputFied.text);
